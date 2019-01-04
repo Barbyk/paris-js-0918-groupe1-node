2e13eb8a559934517497fc85cb674503f9c3caad
eddyte <32571385+eddyte@users.noreply.github.com>
2019-01-04 15:55:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const app = express();
-const port = 3009;
+const port = 3002;
 const connection = require("./conf");
 const bodyParser = require("body-parser");
 const exjwt = require('express-jwt');
